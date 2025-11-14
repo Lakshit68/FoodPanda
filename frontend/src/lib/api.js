eab847b0215a0b73c65cce77436d3e22ac85903b
Lakshit <Lakshitsachdeva25@gmail.com>
2025-11-14 12:06:49 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'https://food-panda-zeta.vercel.app/api',
   withCredentials: true
 })
 
